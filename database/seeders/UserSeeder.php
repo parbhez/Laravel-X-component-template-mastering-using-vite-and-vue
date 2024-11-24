f32bcea4171f96caba798d64a103e56ff88032dd
parbhez <masud.affb@gmail.com>
2024-11-24 23:19:44 +0600

    seeder updated

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -51,6 +51,16 @@ class UserSeeder extends Seeder
                 'remember_token' => Str::random(10), // Corrected line
             ]);
 
+            //For role_sponsorship_manager
+            $masud = User::create([
+                'name' => 'masud',
+                'email' => 'masud@gmail.com',
+                'role' => 'user',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10), // Corrected line
+            ]);
+
 
 
     }
