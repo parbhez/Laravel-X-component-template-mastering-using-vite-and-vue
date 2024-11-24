9d1f857066a99d64fff81eed9d4a9719effcb5ef
parbhez <masud.affb@gmail.com>
2024-11-24 23:18:34 +0600

    added

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -25,10 +25,32 @@ class UserSeeder extends Seeder
             $SuperAdmin = User::create([
                 'name' => 'Super Admin',
                 'email' => 'superadmin@gmail.com',
+                'role' => 'superadmin',
                 'email_verified_at' => now(),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => Str::random(10), // Corrected line
             ]);
+
+            //For role_sponsorship_manager
+            $user1 = User::create([
+                'name' => 'user1',
+                'email' => 'user1@gmail.com',
+                'role' => 'user',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10), // Corrected line
+            ]);
+
+             //For role_sponsorship_manager
+             $user2 = User::create([
+                'name' => 'user2',
+                'email' => 'user2@gmail.com',
+                'role' => 'user',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10), // Corrected line
+            ]);
+
 
 
     }
